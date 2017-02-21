arabobo0109/JS_nylas_email

commit 79a3afacb0416455205d58639ea984bd89b33457

[isomorphic-core] Ensure smtp config exists
Summary:
We previously weren't saving the smtp settings for cloud gmail accounts,
and even though we fixed that, we still need to be able to handle the accounts
that were authed before that fix went out. This diff changes `smtpConfig()` to
always call `credentialsForProvider` instead of depending on what was saved
in the database.

Test Plan: manual

Reviewers: juan, evan

Reviewed By: evan

Differential Revision: https://phab.nylas.com/D3980

packages/isomorphic-core/src/auth-helpers.js
@@ -93,6 +93,7 @@ function credentialsForProvider({provider, settings, email}) {
 
 module.exports = {
   SUPPORTED_PROVIDERS,
+  credentialsForProvider,
   imapAuthRouteConfig() {
     return {
       description: 'Authenticates a new account.',

packages/isomorphic-core/src/models/account.js
@@ -1,7 +1,7 @@
 const atob = require('atob')
 const crypto = require('crypto');
 const {JSONColumn, JSONArrayColumn} = require('../database-types');
-const {SUPPORTED_PROVIDERS} = require('../auth-helpers');
+const {SUPPORTED_PROVIDERS, credentialsForProvider} = require('../auth-helpers');
 
 const {DB_ENCRYPTION_ALGORITHM, DB_ENCRYPTION_PASSWORD} = process.env;
 
@@ -117,10 +117,15 @@ module.exports = (sequelize, Sequelize) => {
       },
 
       smtpConfig() {
-        const {smtp_host, smtp_port, ssl_required} = this.connectionSettings;
+        const {connectionSettings, connectionCredentials} = credentialsForProvider({
+          provider: this.provider,
+          settings: this.decryptedCredentials(),
+          email: this.emailAddress,
+        });
+        const {smtp_host, smtp_port, smtp_username, ssl_required} = connectionSettings;
         let config = {}
-        if (this.connectionSettings.smtp_custom_config) {
-          config = this.connectionSettings.smtp_custom_config
+        if (connectionSettings.smtp_custom_config) {
+          config = connectionSettings.smtp_custom_config
         } else {
           config = {
             host: smtp_host,
@@ -129,15 +134,14 @@ module.exports = (sequelize, Sequelize) => {
           }
         }
         if (this.provider === 'gmail') {
-          const {xoauth2} = this.decryptedCredentials();
+          const {xoauth2} = connectionCredentials;
           if (!xoauth2) {
             throw new Error("Missing XOAuth2 Token")
           }
-          const {imap_username} = this.connectionSettings;
           const token = this.bearerToken(xoauth2);
-          config.auth = { user: imap_username, xoauth2: token }
+          config.auth = { user: smtp_username, xoauth2: token }
         } else if (SUPPORTED_PROVIDERS.has(this.provider)) {
-          const {smtp_username, smtp_password} = this.decryptedCredentials();
+          const {smtp_password} = connectionCredentials
           config.auth = { user: smtp_username, pass: smtp_password}
         } else {
           throw new Error(`${this.provider} not yet supported`)

commit 85dcde9f6107903c846e92f013c0c1a9cecd98a2

[iso-core] Fix Office365 sending from the mail app
Summary:
This fixes a regression introduced in D3980 that prevented sending from working
on Office365 and generic IMAP accounts.

Fixes T7892

Test Plan: manual - we could use unit tests for this but need to set up tests for iso-core first

Reviewers: juan, halla, evan

Reviewed By: halla, evan

Maniphest Tasks: T7892

Differential Revision: https://phab.nylas.com/D4003

packages/isomorphic-core/src/auth-helpers.js
@@ -71,9 +71,9 @@ function credentialsForProvider({provider, settings, email}) {
 
     const connectionCredentials = {
       imap_username: email,
-      imap_password: settings.password,
+      imap_password: settings.password || settings.imap_password,
       smtp_username: email,
-      smtp_password: settings.password,
+      smtp_password: settings.password || settings.smtp_password,
     }
     return {connectionSettings, connectionCredentials}
   } else if (SUPPORTED_PROVIDERS.has(provider)) {

packages/isomorphic-core/src/models/account.js
@@ -117,21 +117,24 @@ module.exports = (sequelize, Sequelize) => {
       },
 
       smtpConfig() {
+        // We always call credentialsForProvider() here because n1Cloud
+        // sometimes needs to send emails for accounts which did not have their
+        // full SMTP settings saved to the database.
         const {connectionSettings, connectionCredentials} = credentialsForProvider({
           provider: this.provider,
-          settings: this.decryptedCredentials(),
+          settings: Object.assign({}, this.decryptedCredentials(), this.connectionSettings),
           email: this.emailAddress,
         });
-        const {smtp_host, smtp_port, smtp_username, ssl_required} = connectionSettings;
-        let config = {}
+        let config;
+        const {smtp_host, smtp_port, ssl_required} = connectionSettings;
         if (connectionSettings.smtp_custom_config) {
-          config = connectionSettings.smtp_custom_config
+          config = connectionSettings.smtp_custom_config;
         } else {
           config = {
             host: smtp_host,
             port: smtp_port,
             secure: ssl_required,
-          }
+          };
         }
         if (this.provider === 'gmail') {
           const {xoauth2} = connectionCredentials;
@@ -139,9 +142,9 @@ module.exports = (sequelize, Sequelize) => {
             throw new Error("Missing XOAuth2 Token")
           }
           const token = this.bearerToken(xoauth2);
-          config.auth = { user: smtp_username, xoauth2: token }
+          config.auth = { user: connectionSettings.smtp_username, xoauth2: token }
         } else if (SUPPORTED_PROVIDERS.has(this.provider)) {
-          const {smtp_password} = connectionCredentials
+          const {smtp_username, smtp_password} = connectionCredentials
           config.auth = { user: smtp_username, pass: smtp_password}
         } else {
           throw new Error(`${this.provider} not yet supported`)